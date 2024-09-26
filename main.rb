a87978eeacf765e237093f0262ffb18cbf5f0546
megafayray <fay.brotherston@gmail.com>
2024-09-26 11:34:36 -0400

    Complete

--- main.rb
+++ main.rb
@@ -1,5 +1,36 @@
-def bubble_sort(numbers)
+# def bubble_sort(numbers)
+#   length = numbers.length-1 
+#   no_change = false
+#   swapped = 0
   
+#   while no_change == false do 
+#     numbers.each_with_index do |numba, index|
+#       if numba[index] > numba[index+1]
+#         #swap their positions
+#         numba[index], numba[index+1] = numba[index+1], numba[index]
+#       else #if they don't need to be swapped
+#         swapped += 1
+#         if swapped == length
+#           break
+#         end
+#         next #is this right?
+#       end
+#     end
+
+#   end
+
+# end
+# 
+def bubble_sort(array)
+  #from the last position to the first, loop:
+  (array.length - 1).downto(0) do |i| #i is the current position in the array
+    #from the first position to the last, loop:
+    0.upto(i - 1) do |j| 
+      next unless array[j] > array[j + 1]
+      array[j], array[j + 1] = array[j + 1], array[j]
+    end
+  end
+  p array
 end
 
 bubble_sort([4,3,78,2,0,2])
